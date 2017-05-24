302b35cefaf1cdea180501992868084edff9876a
Chris Bilson <cbilson@pobox.com>
2017-05-24 08:05:08 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -40,6 +40,68 @@ public class Main {
 		    "Very doubtful"
 		};
 
-		get("/ask-8-ball", (req, res) -> eightBallAnswers[r.nextInt(eightBallAnswers.length)]);
+		get("/ask-8-ball", (req, res) -> eightBallAnswers[r.nextInt(2)]);
+		get("/derivative/:eq", (req, res) -> PowerRule(req.params("eq")));
+	}
+	
+	// 5x^2 -> http://ravencs.club/derivative/5x^2
+	public static String PowerRule(String input) {
+		char[] equ = new char[input.length()];
+		int xpos = 0;
+		int coef = 0;
+		String exp2 ="";
+		int exp = 0;
+		int newcoef;
+		String coef2 = "";
+		
+		//place in character array
+		
+		
+		for (int i = 0; i < input.length(); i++) {
+			equ[i] = input.charAt(i);
+		}
+		if(equ[0]=='x'){
+			System.out.print("1");
+			return;
+		}
+		//this finds the position of x
+		for (int i = 0; i < input.length(); i++) {
+			if (equ[i] == 'x') {
+				xpos = i;
+				//this for loop creates a string for the coefficent, or all numbers before x
+				for (int k = 0; k < xpos; k++) {
+					coef2 += equ[k];
+				}
+				
+				//this changes the string coefficient into an integer
+				coef = Integer.parseInt(coef2);
+				
+				//this detects if x is the final term in the dervitive, if it isn't then it looks for the exponent
+				if (xpos!=input.length()-1) {
+					//if there is a ^, then the for loop creates a string of the exponent
+					for (int j = i + 2; j < input.length(); j++) {
+						exp2 += equ[j];
+					}
+					//this converts the string to a integer
+					exp = Integer.parseInt(exp2);		
+				}
+				else{
+					System.out.print(coef);
+					return;
+				}
+					
+			}
+
+		}
+		
+		if(xpos==0){
+			return "0";
+		}
+		else{
+			newcoef=coef*exp;
+			exp--;
+			return newcoef+"x^"+exp;
+		}
+
 	}
 }
